b2cc985c7ab3b834f743a3890ce4651abd84b9c1
kinako1415 <Owata09@outlook.jp>
2025-04-16 23:46:14 +0900

    tagbuttonのdisableを追加

--- src/layout/TagButton.tsx
+++ src/layout/TagButton.tsx
@@ -5,11 +5,12 @@ import styles from "./TagButton.module.scss";
 type TagButtonProps = {
   children: ReactNode;
   color: "blue" | "green" | "red" | "orange" | "purple";
+  isDisable?: boolean;
 };
 
-export const TagButton = ({ children, color }: TagButtonProps) => {
+export const TagButton = ({ children, color, isDisable }: TagButtonProps) => {
   return (
-    <div className={`${styles.container} ${styles[color]}`}>
+    <div className={`${styles.container} ${isDisable ? styles.disable : styles[color]}`}>
       <div className={styles.text}>{children}</div>
     </div>
   );
