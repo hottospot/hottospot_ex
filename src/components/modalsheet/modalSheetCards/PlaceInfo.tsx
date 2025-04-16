c509c1903768b768dd5ab8621794fa5a39d457b5
kkkk0113 <koto1658@gmail.com>
2025-04-17 00:40:12 +0900

    PlaceInfoコンポーネントのスタイルを追加し、ヘッダーにシェアおよび閉じるボタンを配置。BackgroundおよびTitleコンポーネントのインポート文を整形し、alt属性を追加。

--- src/components/modalsheet/modalSheetCards/PlaceInfo.tsx
+++ src/components/modalsheet/modalSheetCards/PlaceInfo.tsx
@@ -22,21 +22,29 @@ function PlaceInfo() {
   return (
     <div className={styles.container}>
       <div className={styles.whole}>
-        <div className={styles.title}>検索した場所</div>
-        <div className={styles.subtitle}>検索した場所の詳細</div>
+        <div className={styles.header}>
+          <div className={styles.title}>検索した場所</div>
 
-        <div className={styles.buttons}>
-          <div className={styles.share}>
-            <GradationIconButton color="red" onClick={handleShare}>
-              <Icon icon="" style={{ fontSize: '24px', color: '#ffffff' }} />
-            </GradationIconButton>
-          </div>
-          <div className={styles.close}>
-            <GradationIconButton color="gray" onClick={handleClose}>
-              <Icon icon="" style={{ fontSize: '24px', color: '#ffffff' }} />
-            </GradationIconButton>
+          <div className={styles.buttons}>
+            <div className={styles.share}>
+              <GradationIconButton color="red" onClick={handleShare}>
+                <Icon icon="heroicons:arrow-up-tray-16-solid" style={{ fontSize: '22px', color: '#ffffff' }} />
+              </GradationIconButton>
+            </div>
+            <div className={styles.close}>
+              <GradationIconButton color="gray" onClick={handleClose}>
+                <Icon icon="mingcute:close-fill" style={{ fontSize: '22px', color: '#758693' }} />
+              </GradationIconButton>
+            </div>
           </div>
         </div>
+
+        <div className={styles.subtitle}>検索した場所の詳細</div>
+
+        {/* <div className={styles.tags}>
+          <div className={styles.tag1}>○○さんの</div>
+          <div className={styles.tag2}>おしゃれな</div>
+        </div> */}
       </div>
     </div>
   )
