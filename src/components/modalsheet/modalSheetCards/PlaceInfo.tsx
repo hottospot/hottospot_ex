1067a1f7a80e0c99b4e61d580b1c011c2bb8e101
kkkk0113 <koto1658@gmail.com>
2025-04-17 07:30:35 +0900

    PlaceInfoコンポーネントに徒歩、車、電車ボタンのハンドラーを追加し、目的地までの情報を表示するレイアウトを整備

--- src/components/modalsheet/modalSheetCards/PlaceInfo.tsx
+++ src/components/modalsheet/modalSheetCards/PlaceInfo.tsx
@@ -19,6 +19,18 @@ function PlaceInfo() {
     console.log('閉じるボタンが押されました。')
   }
 
+  const handleWalk = () => {
+    console.log('徒歩ボタンが押されました。')
+  }
+
+  const handleCar = () => {
+    console.log('車ボタンが押されました。')
+  }
+
+  const handleTrain = () => {
+    console.log('電車ボタンが押されました。')
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.whole}>
@@ -40,11 +52,41 @@ function PlaceInfo() {
         </div>
 
         <div className={styles.subtitle}>検索した場所の詳細</div>
+      </div>
 
-        {/* <div className={styles.tags}>
-          <div className={styles.tag1}>○○さんの</div>
-          <div className={styles.tag2}>おしゃれな</div>
-        </div> */}
+      <div className={styles.middle}>
+        <div className={styles.middleLeft}>
+          <div className={styles.img}>画像</div>
+        </div>
+        <div className={styles.middleRight}>
+          <div className={styles.targetTitle}>目的地まで</div>
+          <div className={styles.limit}>10km</div>
+          <div className={styles.predict}>予想時間</div>
+
+          <div className={styles.waysButton}>
+            <div className={styles.walk}>
+              <GradationIconButton color="gray" onClick={handleWalk}>
+                <Icon icon="material-symbols:directions-walk-rounded" style={{ fontSize: '15px', color: '#ffffff' }} />
+              </GradationIconButton>
+            </div>
+            <div className={styles.car}>
+              <GradationIconButton color="gray" onClick={handleCar}>
+                <Icon icon="ic:outline-directions-car" style={{ fontSize: '15px', color: '#ffffff' }} />
+              </GradationIconButton>
+            </div>
+            <div className={styles.train}>
+              <GradationIconButton color="gray" onClick={handleTrain}>
+                <Icon icon="material-symbols:train-outline-rounded" style={{ fontSize: '15px', color: '#ffffff' }} />
+              </GradationIconButton>
+            </div>
+          </div>
+
+          <div className={styles.hour}>231時間</div>
+
+          <div className={styles.minutes}>30分</div>
+
+          <div className={styles.url}>参考元リンク</div>
+        </div>
       </div>
     </div>
   )
