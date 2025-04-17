57dc540df38d41ab4d6ec35918efd17949cca670
kinako1415 <Owata09@outlook.jp>
2025-04-17 10:03:07 +0900

    タグを配列で管理

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,4 +1,6 @@
+import { motion } from "framer-motion";
 import { useState } from "react";
+import useMeasure from "react-use-measure";
 
 import { SkipButton } from "../layout/SkipButton";
 import { TagButton } from "../layout/TagButton";
@@ -7,9 +9,19 @@ import styles from "./Search.module.scss";
 
 export const Search = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [ref, { height }] = useMeasure();
+
+  const placeList = ["カフェ", "絶景", "公園", "本屋", "美術館", "スイーツショップ"];
+  const adjectiveList = ["おしゃれな", "かわいい", "スタイリッシュ", "映えてる", "かっこいい"];
+
   return (
-    <div
+    <motion.div
       className={`${styles.container} ${isOpen && styles.openContainer}`}
+      animate={{
+        height: isOpen ? "180px" : `calc(${height}px + 24px)`,
+      }}
+      initial={false}
+      transition={{ duration: 0.16 }}
       onClick={() => {
         setIsOpen(!isOpen);
       }}
@@ -17,7 +29,7 @@ export const Search = () => {
       <div className={styles.placeholder}>見つけたい場所のジャンルを選択しよう !</div>
 
       {isOpen && (
-        <div className={styles.optionGroupWrapper}>
+        <motion.div className={styles.optionGroupWrapper}>
           <div className={styles.optionGroup}>
             <div className={styles.optionGroupHeader}>
               <div className={styles.optionGroupTitle}>形容詞</div>
@@ -25,45 +37,37 @@ export const Search = () => {
             </div>
             <div className={styles.dividerLine} />
             <div className={styles.optionTagList}>
-              <TagButton color="blue">afldjs</TagButton>
-              <TagButton color="red">afasdfasdfldjs</TagButton>
-              <TagButton color="purple">afldjs</TagButton>
-              <TagButton color="blue">afldjfasds</TagButton>
-              <TagButton color="orange">afldjs</TagButton>
+              {placeList.map((place, i) => (
+                <TagButton
+                  key={i}
+                  color="blue"
+                >
+                  {place}
+                </TagButton>
+              ))}
             </div>
           </div>
 
           <div className={styles.optionGroup}>
             <div className={styles.optionGroupHeader}>
-              <div className={styles.optionGroupTitle}>形容詞</div>
+              <div className={styles.optionGroupTitle}>場所</div>
               <SkipButton />
             </div>
             <div className={styles.dividerLine} />
             <div className={styles.optionTagList}>
               <TagButton color="blue">afldjs</TagButton>
-              <TagButton color="red">afasdfasdfldjs</TagButton>
-              <TagButton color="purple">afldjs</TagButton>
-              <TagButton color="blue">afldjfasds</TagButton>
-              <TagButton color="orange">afldjs</TagButton>
+              {adjectiveList.map((adjective, i) => (
+                <TagButton
+                  key={i}
+                  color="blue"
+                >
+                  {adjective}
+                </TagButton>
+              ))}
             </div>
           </div>
-
-          <div className={styles.optionGroup}>
-            <div className={styles.optionGroupHeader}>
-              <div className={styles.optionGroupTitle}>形容詞</div>
-              <SkipButton />
-            </div>
-            <div className={styles.dividerLine} />
-            <div className={styles.optionTagList}>
-              <TagButton color="blue">afldjs</TagButton>
-              <TagButton color="red">afasdfasdfldjs</TagButton>
-              <TagButton color="purple">afldjs</TagButton>
-              <TagButton color="blue">afldjfasds</TagButton>
-              <TagButton color="orange">afldjs</TagButton>
-            </div>
-          </div>
-        </div>
+        </motion.div>
       )}
-    </div>
+    </motion.div>
   );
 };
